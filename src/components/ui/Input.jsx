2e722b71a492d1370369799c4172157192899019
bimurzaew <85867193+bimurzaew@users.noreply.github.com>
2021-10-28 16:11:37 +0300

    s

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./input.module.css";
+import propTypes from "prop-types";
 
 function Input(props) {
   return <input className={styles.input} {...props}/>;
+}
+
+Input.propTypes = {
+  ...props:
 }
 
 export default Input;
